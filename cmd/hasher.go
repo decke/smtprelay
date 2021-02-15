009ae8f73a0fb392799452f0181f9befbef55986
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2021-02-15 00:18:15 -0500

    hasher: Check number of arguments
    This makes for a better user experience than a go segfault.

--- cmd/hasher.go
+++ cmd/hasher.go
@@ -8,11 +8,15 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Usage: hasher PASSWORD")
+		os.Exit(1)
+	}
 	password := os.Args[1]
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		fmt.Println("Error generating hash: %s", err)
+		fmt.Fprintln(os.Stderr, "Error generating hash: %s", err)
 	}
 	fmt.Println(string(hash))
 }
